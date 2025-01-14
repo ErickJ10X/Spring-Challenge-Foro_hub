b786e7c233aa510f3c29d0265671a82973b4817e
Erick Quispe <erickjo10x@gmail.com>
2025-01-14 20:39:59 +0100

    modify table topicos

--- src/main/resources/db/migration/V2__create-table-topicos.sql
+++ src/main/resources/db/migration/V2__create-table-topicos.sql
@@ -1,13 +1,14 @@
 
 create table topicos(
 
-                         id serial not null,
-                         titulo varchar(100) not null,
-                         mensaje varchar(300) not null,
-                         fecha varchar(100) not null,
-                         status boolean not null,
-                         autor varchar(100) not null,
-                         curso varchar(100) not null,
+                        id serial not null,
+                        titulo varchar(100) not null,
+                        mensaje varchar(300) not null,
+                        fecha date not null,
+                        status boolean not null,
+                        autor integer not null,
+                        curso varchar(100) not null,
 
-                         primary key (id)
+                        primary key (id),
+                        foreign key (autor) references usuarios(id)
 );
